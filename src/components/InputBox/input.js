cd13f65df39a1ea406baee41a3a7e1dd49a2c9c5
Phelimon Sarpaning <32636728+PhelimonSarpaning@users.noreply.github.com>
2023-10-03 08:09:28 -0500

    input space

---
+++ src/components/InputBox/input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input = () => {
+    return (
+        <div>
+            <input placeholder='put your notes here'>
+            </input>
+        </div>
+    )
+}
+
+export default Input;
---
+++ src/views/note.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Input from '../components/InputBox/input';
+
+const Note = () => {
+    return(
+        <div>
+            <p>Hello World</p>
+            <Input />
+        </div>
+    )
+}
+
+export default Note;
